1ded83e6997ead7a32fa98e5f5b505cf9ba33b0f
ereadian <ereadian@gmail.com>
2026-08-28 21:48:53 -0700

    add more unit test for context

--- tools/builder/unit-test/HtmlParserContextUnitTest.cs
+++ tools/builder/unit-test/HtmlParserContextUnitTest.cs
@@ -7,8 +7,15 @@ using ereadian.builder.html;
 [ExcludeFromCodeCoverage]
 public sealed class HtmlParserContextUnitTest
 {
+    public enum Location
+    {
+        Start,
+        Middle,
+        End
+    }
+
     [TestMethod]
-    public void HtmlParserContext_Constructor()
+    public void HtmlParserContext_Constructor_AllPropertiesAreSet()
     {
         // Arrange
         Random random = Random.Shared;
@@ -99,33 +106,148 @@ public sealed class HtmlParserContextUnitTest
     }
 
     [TestMethod]
-    [DataRow(0, DisplayName = "At beginning")]
-    [DataRow(1, DisplayName = "In the middle")]
-    [DataRow(2, DisplayName = "At the end")]
-    public void HtmlParserContext_IsEnd(int positionCode)
+    [DataRow(Location.Start, DisplayName = "At beginning")]
+    [DataRow(Location.Middle, DisplayName = "In the middle")]
+    [DataRow(Location.End, DisplayName = "At the end")]
+    public void HtmlParserContext_IsEnd(Location positionCode)
     {
         // Arrange
         Random random = Random.Shared;
         string content = TestUtility.CreateUniqueName("content");
         HtmlParserContext context = CreateContext(random, content);
-        switch(positionCode)
+        context.CurrentPosition = positionCode switch
         {
-            case 0:
-                context.CurrentPosition = 0;
-                break;
-            case 1:
-                context.CurrentPosition = content.Length / 2;
-                break;
-            default:
-                context.CurrentPosition = content.Length;
-                break;
-        }
+            Location.Start => 0,
+            Location.Middle => content.Length / 2,
+            _ => content.Length,
+        };
 
         // Act
         bool isEnd = context.IsEnd();
 
         // Assert
-        Assert.AreEqual(positionCode > 1, isEnd);
+        Assert.AreEqual(positionCode > Location.Middle, isEnd);
+    }
+
+    [TestMethod]
+    [DataRow(Location.Start, DisplayName = "At beginning")]
+    [DataRow(Location.Middle, DisplayName = "In the middle")]
+    [DataRow(Location.End, DisplayName = "At the end")]
+    public void HtmlParserContext_GetCurrentChar_ReturnExpected(Location positionCode)
+    {
+        // Arrange
+        Random random = Random.Shared;
+        int characterCount = random.Next(10, 20);
+        StringBuilder builder = new StringBuilder(characterCount);
+        HashSet<char> used = new();
+        for (int i = 0; i < characterCount; i++)
+        {
+            char c = (char)('a' + random.Next(characterCount));
+            while (used.Contains(c))
+            {
+                c++;
+                if (c > 'z')
+                {
+                    c = 'a';
+                }
+            }
+            _ = builder.Append(c);
+        }
+
+        int position = positionCode switch
+        {
+            Location.Start => 0,
+            Location.Middle => characterCount / 2,
+            _ => characterCount - 1
+        };
+
+        char expected = builder[position];
+        HtmlParserContext context = CreateContext(random, builder.ToString());
+        context.CurrentPosition = position;
+
+        // Act
+        char actual = context.GetCurrentChar();
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    [DataRow(Location.Start, DisplayName = "At beginning")]
+    [DataRow(Location.Middle, DisplayName = "In the middle")]
+    [DataRow(Location.End, DisplayName = "At the end")]
+    public void HtmlParserContext_StartWith_FindExist_ReturnTrue(Location positionCode)
+    {
+        // Arrange
+        Random random = Random.Shared;
+        int prefixCharCount = positionCode == Location.Start ? 0 : random.Next(5, 10);
+        StringBuilder builder = new();
+        AppendRandomString(builder, random, prefixCharCount);
+        string searchValue = TestUtility.CreateUniqueName("value");
+        _ = builder.Append(searchValue);
+        int suffixCharCount = positionCode == Location.End ? 0 : random.Next(5, 10);
+        AppendRandomString(builder, random, suffixCharCount);
+
+        HtmlParserContext context = CreateContext(random, builder.ToString());
+
+        // Arrange
+        bool expected = context.StartsWith(searchValue, prefixCharCount);
+
+        // Assert
+        Assert.IsTrue(expected);
+    }
+
+    [TestMethod]
+    public void HtmlParserContext_StartWith_NotExist_ReturnFalse()
+    {
+        // Arrange
+        Random random = Random.Shared;
+        string searchValue = TestUtility.CreateUniqueName("value");
+        int count =random.Next(searchValue.Length * 2);
+        StringBuilder builder = new (count);
+        _ = builder.Append(searchValue);
+        string content = string.Empty;
+        do
+        {
+            AppendRandomString(builder, random, count);
+            content = builder.ToString();
+        } while(content.IndexOf(searchValue) < 0);
+
+        HtmlParserContext context = CreateContext(random, content);
+
+        // Arrange
+        bool expected = context.StartsWith(searchValue, searchValue.Length);
+
+        // Assert
+        Assert.IsFalse(expected);
+    }
+
+
+    [TestMethod]
+    public void HtmlParserContext_StartWith_NoEnoughSize_ReturnFalse()
+    {
+        // Arrange
+        Random random = Random.Shared;
+        string searchValue = TestUtility.CreateUniqueName("value");
+        int count =random.Next(5, 10);
+        StringBuilder builder = new (count);
+        AppendRandomString(builder, random, count);
+        _ = builder.Append(searchValue.Substring(0, searchValue.Length / 2));
+        HtmlParserContext context = CreateContext(random, builder.ToString());
+
+        // Arrange
+        bool expected = context.StartsWith(searchValue, count);
+
+        // Assert
+        Assert.IsFalse(expected);
+    }
+
+    public static void AppendRandomString(StringBuilder builder, Random random, int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            _ = builder.Append((char)random.Next(' ', '~' - ' '));
+        }
     }
 
     private static HtmlParserContext CreateContext(Random random, string content)
